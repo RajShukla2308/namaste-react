89a6d5013336fc73cdd31ac0387c75a258169411
Raj Shukla <raj@Rajs-MacBook-Air.local>
2024-10-14 22:16:59 +0530

    class1 examples

--- App.js
+++ App.js
@@ -17,3 +17,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //passing a react element inside the root
 //injects the element into DOM
 root.render(container);
+
+
+// Example 2---------------
+
+const structure2 = React.createElement("div",{id:'data-container'},
+    React.createElement('h1',{id:'first-heading'},'This is first heading'),
+    React.createElement('h2',{id: 'second-heading'},'This is second heading')
+)
+root.render(structure2);
+
+// end of Example2--------
